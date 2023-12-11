dffaede6b82fb615e5c6bc68059f441aaea599a7
Walter Ismael Sagastegui Lescano <datasagatrujillo@gmail.com>
2023-12-11 17:22:48 +0100

    Commit 3

--- Principal.java
+++ Principal.java
@@ -1,17 +1,20 @@
-public class Principal {//INICIO CLASE
+public class Principal {// INICIO CLASE
 
-    public static void main(String[] args) { //INICIO MAIN
-       
-		System.out.println("COMMIT 1: LUNES");
+    public static void main(String[] args) { // INICIO MAIN
+
+        System.out.println("COMMIT 1: LUNES");
         int[] vector = new int[10];
 
         System.out.println("COMMIT 2: MARTES");
-        for(int i=0; i<vector.length; i++) {
-            vector[i] = (int)(Math.random() * 6 + 1);
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random() * 6 + 1);
         }
 
+        System.out.println("COMMIT 3: MIERCOLES");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + "   ");
+        }
 
-   
-    }//FINAL MAIN
-    
-}//FIN CLASE
+    }// FINAL MAIN
+
+}// FIN CLASE
