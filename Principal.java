740bc6b58fb3e63291b062eaf748604a876b9656
Walter Ismael Sagastegui Lescano <datasagatrujillo@gmail.com>
2023-12-11 17:01:36 +0100

    Commit 1

---
+++ Principal.java
@@ -0,0 +1,15 @@
+public class Principal {//INICIO CLASE
+
+    public static void main(String[] args) { //INICIO MAIN
+       
+		System.out.println("COMMIT 1: LUNES");
+        int[] vector = new int[10];
+
+
+    
+
+        
+	   
+    }//FINAL MAIN
+    
+}//FIN CLASE
